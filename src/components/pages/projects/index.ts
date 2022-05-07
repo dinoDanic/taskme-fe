62fc1a6f1fa4328fc33e7c736700fd94f23937c1
Dino Danic <dino.danic@gmail.com>
2022-05-07 12:15:33 +0200

    som

--- src/components/pages/projects/index.ts
+++ src/components/pages/projects/index.ts
@@ -1,3 +1,4 @@
 export * from "./my-projects";
 export * from "./my-tasks";
 export * from "./my-subtasks";
+export * from "./single-task";
--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -1,8 +1,29 @@
+import { useQuery } from "@apollo/client";
+import { SingleTask } from "components/pages";
+import { Button, H2, SubHeaderPage } from "components/ui";
+import { useGetAssignedTasksQuery, Task } from "generated/graphql";
 import { NextPage } from "next";
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const Tasks: NextPage = () => {
-  return <div>Tasks</div>;
+  const { data } = useGetAssignedTasksQuery();
+  console.log(data);
+
+  const mapTasks = data?.getAssignedTasks?.map((t) => (
+    <SingleTask key={t.id} {...t} />
+  ));
+
+  return (
+    <Container>
+      <SubHeaderPage>
+        <H2 style={{ margin: 0 }}>Tasks</H2>
+      </SubHeaderPage>
+      {mapTasks}
+    </Container>
+  );
 };
+
+const Container = styled.div``;
 
 export default Tasks;
